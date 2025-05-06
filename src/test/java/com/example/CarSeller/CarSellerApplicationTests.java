461f0e9a1c229e8121aab6e905500e734c10f722
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-06 15:25:48 +0200

    spring init

---
+++ src/test/java/com/example/CarSeller/CarSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarSeller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
