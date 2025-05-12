644e3b704a68160fff2a12939a58af5009c876be
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-12 10:26:26 +0200

    Car repository update new finds done

--- src/main/java/com/example/CarSeller/repositories/CarRepository.java
+++ src/main/java/com/example/CarSeller/repositories/CarRepository.java
@@ -3,8 +3,13 @@ package com.example.CarSeller.repositories;
 import com.example.CarSeller.models.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
 
 public interface CarRepository extends JpaRepository<Car, Integer> {
+    List<Car> findByModel(String model);
+    List<Car> findByBrand(String brand);
+    List<Car> findByEngine(EEngine engine);
+    List<Car> findByPrice(Double price);
 
 }
